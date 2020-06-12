99430d24d8acccd39dd2f1ea1f72b612e4a09957
jdohea <jdohea@gmail.com>
2020-06-12 16:25:23 +0200

    bits of refactoring

--- core/src/Model/PhysicsEngine.java
+++ core/src/Model/PhysicsEngine.java
@@ -5,26 +5,13 @@ public interface PhysicsEngine {
 	
 	public double get_height(double x, double y);
 	public void nextStep();
-	
-
 	public void setPosition(Vector2d v);
 	public void setPosZ(double d);
-	
-
 	public void setVelocity(Vector2d v);
-	
-	public void setMu(double mu);
-	public void setVMax (double vMax);
-	
-
 	public Vector2d getPosition();
 	public double getPosZ();
-	
 	public Vector2d getVelocity();
-	
-//	public void setGoalPosition (Vector2d newGoal);
 	public boolean finish();
-
 	public abstract void set_step_size(double h);
 
 }
