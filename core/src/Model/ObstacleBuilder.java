b61fbe91ee10c15630d0088cf79e089edcba8a27
jojotrieu <jojotrieu@gmail.com>
2020-06-17 17:19:44 +0200

    render Obstacles

--- core/src/Model/ObstacleBuilder.java
+++ core/src/Model/ObstacleBuilder.java
@@ -1,7 +1,14 @@
 package Model;
 
+import Model.Sides.HorizontalSide;
 import Model.Sides.Side;
 import Model.Sides.VerticalSide;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.PuttingCourse;
 
@@ -11,6 +18,8 @@ import com.mygdx.game.PuttingCourse;
 public class ObstacleBuilder {
 
     private static final float MAX_DIS_STEP= .01f;
+
+    private static final float OBSTACLE_HEIGHT = 5;
 
     /**
      * Sets the flag pole object in the course and ready to render
@@ -37,7 +46,7 @@ public class ObstacleBuilder {
      * @param length length of the face, always along x axis
      * @param width length of the side along the y axis
      */
-    public static void makeBox(Vector2 startPos, float length, float width){
+    public static ModelInstance makeBox(Vector2 startPos, float length, float width, ModelBuilder mb){
         Vector2 v1 = new Vector2(startPos.x+ length, startPos.y);
         Vector2 v2 = new Vector2(startPos.x, startPos.y+width);
         Vector2 v3 = new Vector2(startPos.x+length, startPos.y+width);
@@ -50,9 +59,16 @@ public class ObstacleBuilder {
 
         Side s1 = new VerticalSide(startPos, v2);
         Side s2 = new VerticalSide(v1,v3);
-        Side s3 = new
+        Side s3 = new HorizontalSide(startPos, v1);
+        Side s4 = new HorizontalSide(v2,v3);
+        tmp.addSide(s1);
+        tmp.addSide(s2);
+        tmp.addSide(s3);
+        tmp.addSide(s4);
 
-
+        ModelInstance mi = new ModelInstance(mb.createBox(length,OBSTACLE_HEIGHT, width, new Material(ColorAttribute.createDiffuse(Color.GRAY)), VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal) );
+        mi.transform.setToTranslation(startPos.x+length/2,0, startPos.y+width/2);
+        return mi;
 
     }
 
